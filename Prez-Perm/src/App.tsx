ad1f0f28ce6e5df845c70c3260267c278e4bc846
XxCaptainNinjaxX <captainninjaflash@gmail.com>
2026-02-17 19:42:35 -0500

    Created Sidebar - moved down header tag so corresponding pages will be shown below it - moved sidebar and header in a div to fix smushing

--- Prez-Perm/src/App.tsx
+++ Prez-Perm/src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 import Home from "./Pages/Main-Pages/Home/Home";
 import About from "./Pages/Main-Pages/About/About";
@@ -11,13 +11,21 @@ import Donation from "./Pages/Main-Pages/Donation/Donation";
 export default function App() {
   return (
     <>
-      {/* <Routes>
-        <Route path="/About" element={<About />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Donations" element={<Donation />} />
-      </Routes> */}
       <Header />
-      <h1> test test</h1>
+
+      <div className="page-body">
+        <Sidebar />
+
+        <main className="main-content">
+          <Routes>
+            <Route path="/About" element={<About />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Donations" element={<Donation />} />
+          </Routes>
+
+          <h1>test test</h1>
+        </main>
+      </div>
     </>
   );
 }
