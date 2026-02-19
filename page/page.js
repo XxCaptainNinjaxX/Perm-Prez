b3b500d933d5aa1fdabd3d31ba7c893e758b0bd9
XxCaptainNinjaxX <captainninjaflash@gmail.com>
2026-02-19 11:35:30 -0500

    READY FOR PUBLISH - removed all old files

--- page/page.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let toggleButton = document.getElementById("sidebarToggle");
-  let sidebar = document.getElementById("sidebar");
-
-  toggleButton.addEventListener("click", function () {
-    sidebar.classList.toggle("closed");
-  });
-});
